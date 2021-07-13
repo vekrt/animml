4ffa707bee3df12e3888ff8c4b784aef76da2b9b
KT <49154901+vekrt@users.noreply.github.com>
2021-07-14 00:06:25 +0200

    Add files via upload
    Updated tree data

--- Stat/hierarchical_linkage.nim
+++ Stat/hierarchical_linkage.nim
@@ -1,23 +1,43 @@
 import arraymancer
 import utils
+import strutils
 
 type 
     Euclidean* = object
+    Manhattan* = object
     Single_linkage* = object
     Complete_linkage* = object
     Weighted_linkage* = object
-    Node*[T] = ref object
-        left*, right*: Node[T]
-        value*: T
 
-proc newNode[T](data: T): Node[T] =
-    new(result)
-    result.data = data
+    Node_kind* = enum
+     Leaf, Branch
+
+    Node*[T] = ref object
+        case kind*: Node_kind
+        of Branch:
+            children*: seq[Node[T]]
+            distance*: T
+        of Leaf:
+            index*: int
+
+proc disp[T: SomeNumber](node: Node[T], level = 0): void =
+    let shift = "\t".repeat(level)
+    if node.kind == Leaf:
+        echo shift, "Index: ", node.index
+    else:
+        echo shift, "Distance: ", node.distance
+        for c in node.children:
+            disp(c, level + 1)
 
 proc pairwise[T: SomeFloat](metric: typedesc[Euclidean]; x,y: Tensor[T]): T =
     let diff = x - y
 
     return sqrt(sum(diff *. diff))
+
+proc pairwise[T: SomeFloat](metric: typedesc[Manhattan]; x,y: Tensor[T]): T =
+    let diff = x - y
+
+    return sum(abs(diff))
 
 proc distance_matrix[T: SomeFloat](metric: typedesc[Euclidean]; X: Tensor[T]): Tensor[T] =
     let n = X.shape[0].int
@@ -64,20 +84,35 @@ proc HClustering*[T: SomeNumber](linkage: typedesc[Single_linkage or Complete_li
                                     dist: Tensor[T]): T =
     var dist_matrix = dist
     var n = dist_matrix.shape[0].int
+    var nodes = newSeq[Node[T]](n)
+    var main_indices = [0..n-1].toTensor()
+
+    for i in 0..n-1:
+        nodes[i] = Node[T](kind: Leaf, index: i)
 
     while n > 1:
         let indices = find_min(dist_matrix)
-        echo "cluster ", indices
-        echo dist_matrix
+        #echo "cluster ", indices
+        var children = newSeq[Node[T]](len(indices))
+        for i in 0..high(indices):
+            children[i] = nodes[indices[i]]
+
+        nodes[indices[0]] = Node[T](kind: Branch, distance: dist_matrix[indices[0], indices[1]] / 2.0, children: children)
+
         let new_cluster = linkage.comparison(dist_matrix[_, indices], axis=1)
         dist_matrix[_, indices[0]] = new_cluster
         dist_matrix[indices[0], _] = transpose(new_cluster)
+        #nodes[indices[0]] = Node[T](distance: dist_matrix[indices[0], indices[1]]/2.0, index: -1, children: nodes[indices])
         var keep_idx = ones[T](n).astype(bool)
         for ii in 1..high(indices):
             keep_idx[indices[ii]] = false
+            nodes.delete(indices[ii])
         dist_matrix = dist_matrix.masked_axis_select(keep_idx, axis=0).masked_axis_select(keep_idx, axis=1)
         #echo dist_matrix.shape
         n = dist_matrix.shape[0].int
+
+    disp(nodes[0])
+
 
 proc HClustering*[T: SomeNumber](linkage: typedesc[Single_linkage or Complete_linkage or Weighted_linkage];
                                   metric: typedesc[Euclidean];
@@ -92,8 +127,8 @@ var y = [-1.0, -1.0].toTensor()
 var X = [-0.3616294, -0.26037983, 1.26567644, 2.34676237, -0.03166053, 0.68876382
 , -1.36468868, 0.53570823, -1.35740064, 0.57883562].toTensor().reshape(10,1)
 
-echo "Clustering"
-discard HClustering(Single_linkage, Euclidean, X)
+#echo "Clustering"
+#discard HClustering(Single_linkage, Euclidean, X)
 
 var dist = [0,17,21,31,23,
             17,0,30,34,21,
@@ -103,3 +138,14 @@ var dist = [0,17,21,31,23,
 
 echo "Distance matrix"
 discard HClustering(Weighted_linkage, dist)
+#var a1,a2 : Node[float]
+#var b : Node[float]
+#var b1 : Node[float]
+#var c : Node[float]
+#a1 = Node[float](distance: 2.0, children: @[])
+#a2 = Node[float](distance: 1.0, children: @[])
+#b = Node[float](distance: 3.0, children: @[a1, a2])
+#b1 = Node[float](distance: 3.5, children: @[])
+#c = Node[float](distance: 4.0, children: @[b, b1])
+
+#disp(c)
